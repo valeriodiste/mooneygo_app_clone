a138ae170e79b01dd99f4a0555199251f58dcd4a
valeriodiste <valeriodstfn@gmail.com>
2024-04-24 18:14:18 +0200

    Updated files

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 
-// Wait for the document to be ready
-$(document).ready(function () {
+// Function used to show the dimensions of the screen, the window, and the document on the page at document load
+function showDimensions() {
 
 	// Add a div with, inside, the dimensions of the screen, the window, and the document
 	$('#main').append('<div id="dimensions" style="z-index: 10; font-size: 24px; color: #000000;"></div>');
@@ -21,6 +21,42 @@ $(document).ready(function () {
 	// Update the dimensions when the page is loaded
 	updateDimensions();
 
-	console.log('script.js loaded');
+}
+
+// Functoin to move to the "ticket" page
+function activateTicket() {
+
+	console.log("Ticket activated!");
+
+}
+
+// Wait for the document to be ready
+$(document).ready(function () {
+
+	// Call the function to show the screen, window, and document dimensions on the page
+	// showDimensions();
+
+	// Update the ticket's activation date overlay text
+	let activationDateElement = $("#activation-date");
+	// Get the date to display (current day, some minutes before the current time)
+	let minutesBefore = 14;
+	let dateToUse = new Date();
+	dateToUse.setMinutes(dateToUse.getMinutes() - minutesBefore);
+	// Format the date as "dd/mm/yyyy - hh:mm"
+	let dateText = dateToUse.getDate() + "/" + (dateToUse.getMonth() + 1) + "/" + dateToUse.getFullYear() + " - " + dateToUse.getHours() + ":" + dateToUse.getMinutes();
+	activationDateElement.text(dateText);
+
+	// Add click functions to the ticket button
+	let ticketClickOverlay = $("#ticket-button");
+	// On tap down, set the ticket overlay to have an opacity of 0.3
+	ticketClickOverlay.on("touchstart", function () {
+		ticketClickOverlay.css("opacity", "0.3");
+	});
+	// On tap up, set the ticket overlay to have an opacity of 0 and "activate" the ticket
+	ticketClickOverlay.on("touchend", function () {
+		ticketClickOverlay.css("opacity", "0");
+		activateTicket();
+	});
+
 
 });
